b0f6f7581c6d52bca0715560e14177bc918df99b
joeban <joeanddaneil5@gmail.com>
2024-12-06 17:34:44 +0800

    test: add console to see error

--- src/routes/demo/lucia/login/+page.server.ts
+++ src/routes/demo/lucia/login/+page.server.ts
@@ -72,12 +72,13 @@ export const actions: Actions = {
 		});
 
 		try {
-			await db.insert(table.user).values({ id: userId, username, passwordHash });
-
+			const res = await db.insert(table.user).values({ id: userId, username, passwordHash });
+			console.log('res', res);
 			const sessionToken = auth.generateSessionToken();
 			const session = await auth.createSession(sessionToken, userId);
 			auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
 		} catch (e) {
+			console.error('e', e);
 			return fail(500, { message: 'An error has occurred' });
 		}
 		return redirect(302, '/demo/lucia');
